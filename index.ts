41374bbce749f54eb684f22baa8df4f8e2f924e3
tungbt94 <tungbt94@gmail.com>
2017-08-15 15:07:29 +0700

    config to deploy openshift

--- index.ts
+++ index.ts
@@ -21,6 +21,13 @@ app.post('/api/qoute/add', async (req, res) => {
   addQuote(req, res);
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000');
+// app.listen(3000, function () {
+//   console.log('listening on port 3000');
+// });
+
+var server_port = +process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
